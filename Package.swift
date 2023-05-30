8cebdc8c0d9bb4de0be07d971f3f82e941fa2669
Ryan Burgoyne <ryan@skyglass.com>
2023-05-30 11:01:57 -0600

    Adopt Skyglass fork of Storage

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/supabase-community/gotrue-swift", from: "1.0.0"),
-    .package(url: "https://github.com/supabase-community/storage-swift.git", from: "0.1.1"),
+    .package(url: "git@github.com:skyglassapp/storage-swift.git", branch: "main"),
     .package(url: "https://github.com/supabase-community/realtime-swift.git", from: "0.0.2"),
     .package(url: "https://github.com/supabase-community/postgrest-swift", from: "1.0.0"),
     .package(url: "https://github.com/supabase-community/functions-swift", from: "1.0.0"),
